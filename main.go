79701879afb1f921eb357747368d9294ddb177e5
crushten <34463457+crushten@users.noreply.github.com>
2022-07-26 17:47:01 -0400

    #2 / fixed timestamp

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ import (
 
 // message represents data about a message post
 type message struct {
-	ID        int       `json:"id"`
-	Message   string    `json:"message"`
-	Timestamp time.Time `json:"timestamp"`
+	ID        int    `json:"id"`
+	Message   string `json:"message"`
+	Timestamp int64  `json:"timestamp"`
 }
+
+// Need struct to be in64 to use this
+var t = time.Now().UnixMilli()
 
 // message slice to seed data
 var messages = []message{
-	{ID: 1, Message: "Automate all the things!"},
+	{ID: 1, Message: "Automate all the things!", Timestamp: t},
 }
 
 // getMessages responds with list of all the messages as json
