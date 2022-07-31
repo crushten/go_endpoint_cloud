ef888c5520fdff0f4fdb5998cd2f8c66c11f615c
crushten <34463457+crushten@users.noreply.github.com>
2022-07-31 17:11:52 -0400

    #10 / tried to fix docker context

--- terraform/docker_repo.tf
+++ terraform/docker_repo.tf
@@ -7,20 +7,12 @@ resource "aws_ecr_repository" "demo-repository" {
     }
 }
 
-provider "docker" {
-  registry_auth {
-    address  = local.aws_ecr_url
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
-  }
-}
-
 ## Build docker images and push to ECR
 resource "docker_registry_image" "go_endpoint_cloud" {
     name = "${aws_ecr_repository.demo-repository.repository_url}:latest"
 
     build {
-        context = ".."
-        dockerfile = "Dockerfile"
+        context = "../application"
+        #dockerfile = "Dockerfile"
     }  
 }
