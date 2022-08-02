ab64fda2c476bf65befc13c9c32cbe3762515617
crushten <34463457+crushten@users.noreply.github.com>
2022-08-01 21:05:30 -0400

    #11 / rearranged k8s things

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,48 +1,79 @@
+resource "kubernetes_namespace" "godemo" {
+  metadata {
+    name = "godemo"
+  }
+
+}
+
 resource "kubernetes_deployment" "go" {
   metadata {
     name = "go-deployment"
+    namespace = kubernetes_namespace.godemo.metadata.0.name
     labels = {
       app  = "go_endpoint_cloud"
-    }
-  }
+    } //labels
+  } //metadata
+
   spec {
     replicas = 1
     selector {
       match_labels = {
         app  = "go_endpoint_cloud"
-      }
-    }
+      } //selector
+    } //spec
+
     template {
       metadata {
         labels = {
           app  = "go_endpoint_cloud"
-        }
-      }
+        } //labels
+      } //metadata
+
     spec {
       container {
         image = "${aws_ecr_repository.demo-repository.repository_url}:latest"
         name  = "go-endpoint-cloud-container"
-      port {
-            container_port = 8080
-      }
-      }
-    }
-    }
-  }
-}
+        port {
+          container_port = 8080
+        } //port
+        resources {
+          limits = {
+          cpu    = "0.5"
+          memory = "512Mi"
+        } //limits
+        requests = {
+          cpu    = "250m"
+          memory = "50Mi"
+        } //requests
+        } //container resources 
+        liveness_probe {
+          http_get {
+            path = "/healthcheck"
+            port = 8080
+          } //http get
+        initial_delay_seconds = 5
+        period_seconds        = 15
+        } //liveness probe
+      } //container
+    } //spec
+    } //template
+  } //s[ec]
+} //resource deployment
+
 resource "kubernetes_service" "go" {
   depends_on = [kubernetes_deployment.go]
   metadata {
     name = "go-endpoint-cloud-service" //doesnt seem to like underscores for some reason
-  }
+    namespace = kubernetes_namespace.godemo.metadata.0.name
+  } //metadata
   spec {
     selector = {
-      app = "go_endpoint_cloud"
+      app = kubernetes_deployment.go.spec.0.template.0.metadata[0].labels.app
     }
     port {
       port        = 8080
       target_port = 8080
     }
     type = "LoadBalancer"
-  }
-}
+  } //spec
+} //resource service
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "eks_managed_node_group_mgmt_one" {
-  name_prefix = "eks_managed_node_group_mgmt_one"
+resource "aws_security_group" "eks_managed_node_group_one" {
+  name_prefix = "eks_managed_node_group_one"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -13,8 +13,8 @@ resource "aws_security_group" "eks_managed_node_group_mgmt_one" {
   }
 }
 
-resource "aws_security_group" "all_node_mgmt" {
-  name_prefix = "all_node_management"
+resource "aws_security_group" "eks_managed_node_group_two" {
+  name_prefix = "eks_managed_node_group_two"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -23,8 +23,6 @@ resource "aws_security_group" "all_node_mgmt" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
       "192.168.0.0/16",
     ]
   }
