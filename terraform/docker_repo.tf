a9f28e22f6ce5044140c4856116dad53d69d9267
crushten <34463457+crushten@users.noreply.github.com>
2022-08-06 18:25:56 -0400

    #25 / added delete option for ecr

--- terraform/docker_repo.tf
+++ terraform/docker_repo.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "demo-repository" {
   name                 = "demo-repo"
   image_tag_mutability = "IMMUTABLE"
+  force_delete = true
 
     image_scanning_configuration {
       scan_on_push = true
